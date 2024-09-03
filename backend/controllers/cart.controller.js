d582dfc0cab912b4738984bf3a79280b8b2b2ca5
Vishnu V V <sonic512-dnetz>
2024-09-03 08:10:07 +0530

    Added Npm Install command to Build

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,16 +1,22 @@
-import Wardrobe from "../models/Wardrobe.model.js"
+import Wardrobe from "../models/Wardrobe.model.js";
 
-export const saveToCart = async(req, res) => {
+export const saveToCart = async (req, res) => {
   try {
     console.log(req.body);
-    const {cupboardLength, cupboardWidth1, cupboardWidth2, material} = req.body;
-    const newCart = await Wardrobe.create({cupboardLength,cupboardWidth1,cupboardWidth2, material})
+    const { cupboardLength, cupboardWidth1, cupboardWidth2, material } =
+      req.body;
+    const newCart = await Wardrobe.create({
+      cupboardLength,
+      cupboardWidth1,
+      cupboardWidth2,
+      material,
+    });
     newCart.save();
-    console.log(newCart)
+    console.log(newCart);
     res.status(200).json({ error: false, message: "Saved" });
   } catch (error) {
     res
       .status(404)
       .json({ message: "SERVER_ERROR_WARDROBE_SAVE", error: true });
   }
-}
+};
